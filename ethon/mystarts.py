2a1174ed7302f5bbb48e8050da3d7f3032c66836
Hussien <158073869+Hjwad@users.noreply.github.com>
2024-05-04 06:03:58 +0300

    Update mystarts.py

--- ethon/mystarts.py
+++ ethon/mystarts.py
@@ -6,8 +6,6 @@ from telethon import events, Button
 async def start_srb(event, st):
     await event.reply(st, 
                       buttons=[
-                              [Button.inline("SET THUMB.", data="set"),
-                               Button.inline("REM THUMB.", data="rem")],
                               [Button.url("DEV", url="t.me/mmmsc")]])
                               
     
